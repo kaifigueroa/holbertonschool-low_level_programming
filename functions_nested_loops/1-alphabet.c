07db72974359532dd8961b76c98fd283533a85bc
kaifigueroa <krystalnfigueroa@gmail.com>
2024-06-19 11:27:40 -0400

    fmocijwqof

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,13 @@
+/*
+ * alphabet.c
+ */
+
 #include "main.h"
 
 /**
- * print_alphabet
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
+
 void print_alphabet(void)
 {
 	char letter;
