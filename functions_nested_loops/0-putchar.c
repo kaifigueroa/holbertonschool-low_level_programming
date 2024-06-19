5094afd63d60b525c79cde0047f71a23623c062b
kaifigueroa <krystalnfigueroa@gmail.com>
2024-06-19 11:19:43 -0400

    rcmfjoqjfoq

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
-/* print puthcar*/
-
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("_putchar/n");
-	return(0);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+    return (0);
 }
